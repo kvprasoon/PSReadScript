214126bb4dd4687712ee2b83c213f5794b14da80
PRASOON KARUNAN V <kvprasoon@users.noreply.github.com>
2016-08-15 12:33:33 +0530

    Update PSWatcher.psm1

--- PSWatcher/PSWatcher.psm1
+++ PSWatcher/PSWatcher.psm1
@@ -1,10 +1,11 @@
+#requires -version 3
 <#
 .Synopsis
 This cmdlet shows the specified script by adding line numbers and error occured lines and more details if required.
 .Description
 This cmdlet shows the specified script by adding line numbers and error occured lines as per the parameters specified.
 This cmdlet has three parameter sets and parameter '-Script' is the only Mandatory parameter.This functionality will 
-be handy in case of Administration using consol only option to analyze error occuring areas of a script.
+be handy in case of Administration using consol only option to analyse error occurng areas of a script.
 .Example
 PS C:\> Watch-PSScript -Script .\test.ps1
 
@@ -165,7 +166,8 @@ Process{
     }
 
 }
-end{
+end
+{
     & $ClearIt          
     $Script:Output | Format-Table  -Wrap
     $ErrorActionPreference = $ErrorActionPreferenceBak
