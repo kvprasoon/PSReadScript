8ea3683253804dab9b0b2a4be809b4ee3d9ebb2f
kvprasoon <kvprasoon@live.in>
2017-05-28 18:55:53 +0530

    Code formatting and alias export

--- PSReadScript/Main/Read-PSFile.ps1
+++ PSReadScript/Main/Read-PSFile.ps1
@@ -24,67 +24,62 @@ Reads all .ml files under -path c:\Files and outputs the occurence of the -Strin
 
 #requires -version 3
 #>
-Function Read-PSFile
-{
-[Alias('rpf','finn')]
-Param(
-#String to Find inside the file
-[Parameter(Mandatory , Position = 0)]
-[String]$String,
+Function Read-PSFile {
+    [Alias('rpf', 'finn')]
+    Param(
+        #String to Find inside the file
+        [Parameter(Mandatory , Position = 0)]
+        [String]$String,
 
-#Path to search the file
-[Parameter(Position = 1)]
-[String]$LookupPath = '.',
+        #Path to search the file
+        [Parameter(Position = 1)]
+        [String]$LookupPath = '.',
 
-#Type of the file (extension eg:- .ps1 , .xml)
-[Parameter(Position = 2)]
-[String]$Type = '.ps1',
+        #Type of the file (extension eg:- .ps1 , .xml)
+        [Parameter(Position = 2)]
+        [String]$Type = '.ps1',
 
-#Recurse option
-[Switch]$Recurse
-)
+        #Recurse option
+        [Switch]$Recurse
+    )
 
 
-Begin{
+    Begin {
 
-	if(-not (Test-Path -Path $LookupPath)){
+        if (-not (Test-Path -Path $LookupPath)) {
 	
-		throw "Unable to find path '$LookupPath'"
+            throw "Unable to find path '$LookupPath'"
 	
-	}
-
-}
-
-Process{
-
-$String = $String.Replace('\','\\')
-
-Get-ChildItem -Path $LookupPath -File -Filter "*$Type" -Recurse:($Recurse.IsPresent) -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName |
-    ForEach-Object -Process {
-    if(Get-Content $_ | Select-String -Pattern $String)
-    {
-        Write-Host -ForegroundColor Green "`n$_"
-        Write-Host -ForegroundColor Green ('=' * ($Host.UI.RawUI.WindowSize.width))
-        $Count = 0
-        Get-Content $_ | ForEach-Object -Process {
-                $Count++
-                [String]$In = $_ | Select-String -Pattern $String
-                if($In)
-                {
-                    if($PSVersionTable.PSVersion.Major -ge 5)
-                    {
-                        Write-Host "$Count" -ForegroundColor Magenta -NoNewline
-                        Write-Host ": $($In.Trim())"
-                    }
-                    else
-                    {
-                        Write-Host "$Count : $($In.Trim())"
-                    }
-                }
-
-                }
         }
+
     }
 
-}
+    Process {
+
+        $String = $String.Replace('\', '\\')
+
+        Get-ChildItem -Path $LookupPath -File -Filter "*$Type" -Recurse:($Recurse.IsPresent) -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName |
+            ForEach-Object -Process {
+            if (Get-Content $_ | Select-String -Pattern $String) {
+                Write-Host -ForegroundColor Green "`n$_"
+                Write-Host -ForegroundColor Green ('=' * ($Host.UI.RawUI.WindowSize.width))
+                $Count = 0
+                Get-Content $_ | ForEach-Object -Process {
+                    $Count++
+                    [String]$In = $_ | Select-String -Pattern $String
+                    if ($In) {
+                        if ($PSVersionTable.PSVersion.Major -ge 5) {
+                            Write-Host "$Count" -ForegroundColor Magenta -NoNewline
+                            Write-Host ": $($In.Trim())"
+                        }
+                        else {
+                            Write-Host "$Count : $($In.Trim())"
+                        }
+                    }
+
+                }
+            }
+        }
+
+    }
 }
--- PSReadScript/PSReadScript.psm1
+++ PSReadScript/PSReadScript.psm1
@@ -2,17 +2,17 @@
 $MainScript = Get-ChildItem -Path "$PSScriptRoot\Main\*.ps1" -ErrorAction SilentlyContinue
 
 
-Foreach($Function in $MainScript)
-{
-    try
-    {
+Foreach ($Function in $MainScript) {
+    try {
         # Functions are loaded here
         . $Function.FullName
     }
-    Catch
-    {
+    Catch {
         Write-Error -Message "Failed to Import function $($Function.Fullname)"
     }
 }
 
-Export-ModuleMember -Function $MainScript.BaseName
+
+
+
+Export-ModuleMember -Function $MainScript.BaseName -Alias 'finn', 'rpf', 'rps', 'fps' -Verbose
